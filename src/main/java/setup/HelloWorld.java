375a2084a69c9f4d130b76dadd399f2cf3ecbfba
User <vlada-sin1999@mail.ru>
2018-11-10 14:51:43 +0300

    Commit 1

--- src/main/java/setup/HelloWorld.java
+++ src/main/java/setup/HelloWorld.java
@@ -3,6 +3,6 @@ package setup;
 public class HelloWorld {
 
     public static void main (String[] args){
-        System.out.println("trtrtrtrtr");
+        System.out.println("tttttttttt");
     }
 }
